5a6fbc06f9a77d40f1dfaf448abe12eae96961e1
garamayo <gisella.aramayo@besysoft.com>
2025-09-21 20:59:33 -0300

    Add build for gh-pages

--- src/app/gifs/components/footer/footer.component.ts
+++
@@ -1,8 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'gifs-footer',
-  imports: [],
-  templateUrl: './footer.component.html'
-})
-export class FooterComponent { }
--- src/environments/environment.development.ts
+++
@@ -1,8 +0,0 @@
-export const environment = {
-  production: true,
-  companyName: 'Gifs',
-  companyName2: 'App',
-  companySlogan: 'Maneja tus gifs',
-  giphyApiKey: '01qBgeaB6HtSiTYzQDIW1gAHRUfZE3qP',
-  giphyUrl: 'https://api.giphy.com/v1',
-};
